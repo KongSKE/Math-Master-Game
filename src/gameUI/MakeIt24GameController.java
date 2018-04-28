008b36519150f2f16cc63c935767934b6c6adced
dacharat <dacharat.p@ku.th>
2018-04-28 17:50:19 +0700

    :1234: Add limit question to MakeIt24 game

--- src/gameUI/MakeIt24GameController.java
+++ src/gameUI/MakeIt24GameController.java
@@ -1,11 +1,17 @@
 package gameUI;
 
+import java.util.Optional;
+
 import org.matheclipse.core.eval.ExprEvaluator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Alert.AlertType;
+import javafx.stage.Stage;
 import makeIt24Game.MakeIt24;
 
 public class MakeIt24GameController {
@@ -41,6 +47,7 @@ public class MakeIt24GameController {
 
 	private MakeIt24 make24;
 	private int bracket;
+	private int question;
 	private ExprEvaluator e;
 
 	public void initialize() {
@@ -62,22 +69,28 @@ public class MakeIt24GameController {
 
 		make24 = new MakeIt24();
 		e = new ExprEvaluator();
+		question = 0;
 		getAllNumber();
 	}
 
 	public void getAllNumber() {
 		bracket = 1;
-		resultLabel.setText("(");
-		make24.getQuestion();
-		number1Button.setText(make24.getNumber1() + "");
-		number2Button.setText(make24.getNumber2() + "");
-		number3Button.setText(make24.getNumber3() + "");
-		number4Button.setText(make24.getNumber4() + "");
+		if (question < 2) {
+			resultLabel.setText("(");
+			make24.getQuestion();
+			number1Button.setText(make24.getNumber1() + "");
+			number2Button.setText(make24.getNumber2() + "");
+			number3Button.setText(make24.getNumber3() + "");
+			number4Button.setText(make24.getNumber4() + "");
 
-		number1Button.setVisible(true);
-		number2Button.setVisible(true);
-		number3Button.setVisible(true);
-		number4Button.setVisible(true);
+			number1Button.setVisible(true);
+			number2Button.setVisible(true);
+			number3Button.setVisible(true);
+			number4Button.setVisible(true);
+			
+			question++;
+		}else 
+			gameEnd();
 	}
 
 	public void onOperationButtonClick(ActionEvent event) {
@@ -106,17 +119,17 @@ public class MakeIt24GameController {
 		String output = oldInput + newInput;
 		if (!number1Button.isVisible() && !number2Button.isVisible() && !number3Button.isVisible()
 				&& !number4Button.isVisible()) {
-			System.out.println("555555");
 			if (bracket > 0) {
-				for(int i = 0; i < bracket; i++) {
+				for (int i = 0; i < bracket; i++) {
 					output += ")";
 				}
 			}
 			resultLabel.setText(output = e.evaluate(output) + "");
-			if(output.equals("24"))
+			if (output.equals("24"))
 				correctLabel.setText("Corect!!");
 			else
 				correctLabel.setText("Wrong!!");
+			getAllNumber();
 		} else {
 			resultLabel.setText(output);
 		}
@@ -128,6 +141,29 @@ public class MakeIt24GameController {
 
 	public void onDeleteButtonClicked(ActionEvent event) {
 		getAllNumber();
+	}
+
+	public void backToHome() {
+		GameUISceneChange.CHOOSEMINIGAME.changeScene((Stage) number1Button.getScene().getWindow());
+	}
+
+	public void gameEnd() {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("Confirmation Dialog with Custom Actions");
+		alert.setHeaderText("Look, a Confirmation Dialog with Custom Actions");
+		alert.setContentText("Choose your option.");
+
+		ButtonType buttonTypeOne = new ButtonType("Play this again");
+		ButtonType buttonTypeTwo = new ButtonType("Back to Home");
+
+		alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo);
+
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == buttonTypeOne) {
+			initialize();
+		} else if (result.get() == buttonTypeTwo) {
+			backToHome();
+		}
 	}
 }
 // ÷
