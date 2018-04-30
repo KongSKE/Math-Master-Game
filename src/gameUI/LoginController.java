f37dcba6f501fa7be297e13d66d60df55d43d019
Varit Assavavisidchai <varit.as@ku.th>
2018-04-30 14:28:20 +0700

    add

--- src/gameUI/LoginController.java
+++ src/gameUI/LoginController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -16,7 +17,10 @@ public class LoginController {
 	TextField username;
 	
 	@FXML
-	TextField password;
+	PasswordField password;
+	
+	@FXML
+	TextField passtext;
 	
 	@FXML
 	Button register;
@@ -30,10 +34,39 @@ public class LoginController {
 	@FXML
 	public void initialize() {
 		login.setOnAction(this::onLoginClicked);
-		
+		register.setOnAction(this::onRegisterClicked);
+		forget.setOnAction(this::onForgetClicked);
+		showpass.setOnAction(this::onCheckBoxClicked);
+		username.setPromptText("Your username");
+		password.setPromptText("Your password");
+		passtext.setVisible(false);
 	}
 	
 	public void onLoginClicked(ActionEvent event) {
 		GameUISceneChange.CHOOSEMINIGAME.changeScene((Stage)login.getScene().getWindow());
+	}
+	
+	public void onCheckBoxClicked(ActionEvent event) {
+		if(showpass.isSelected()) {
+			password.setVisible(false);
+			setTextShowPassword(password.getText().trim());
+			passtext.setVisible(true);
+		}else {
+			passtext.setVisible(false);
+			setTextShowPassword(password.getText().trim());
+			password.setVisible(true);
+		}
+	}
+	
+	public void onRegisterClicked(ActionEvent event) {
+		GameUISceneChange.REGISTER.changeScene((Stage)register.getScene().getWindow());
+	}
+	
+	public void setTextShowPassword(String pass) {
+		passtext.setText(pass);
+	}
+	
+	public void onForgetClicked(ActionEvent evnet) {
+		
 	}
 }
--- src/gameUI/RegisterController.java
+++ src/gameUI/RegisterController.java
@@ -3,57 +3,89 @@ package gameUI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class RegisterController {
-	
+
 	@FXML
 	Button next;
-	
+
 	@FXML
 	Button cancel;
-	
+
 	@FXML
 	TextField username;
-	
+
 	@FXML
-	TextField password;
-	
+	PasswordField password;
+
 	@FXML
-	TextField repassword;
-	
+	PasswordField repassword;
+
+	@FXML
+	Label checkusername;
+
+	@FXML
+	Label checkpass;
+
+	@FXML
+	Label checkrepass;
+
 	@FXML
 	public void initialize() {
 		next.setOnAction(this::onNextButton);
 		cancel.setOnAction(this::onCancelButton);
+		username.setPromptText("Insert username");
+		password.setPromptText("Insert password");
+		repassword.setPromptText("Insert repassword");
+		checkusername.setStyle("-fx-text-inner-color: red;");
+		checkpass.setStyle("-fx-text-inner-color: red;");
+		checkrepass.setStyle("-fx-text-inner-color: red;");
+		checkusername.setVisible(true);
+		checkpass.setVisible(true);
+		checkrepass.setVisible(true);
 	}
-	
+
 	public void onNextButton(ActionEvent event) {
 		String name = username.getText().trim();
 		String pass = password.getText().trim();
 		String repass = repassword.getText().trim();
-		if(name.equals("") && pass.equals("") && repass.equals("")) {
-			GameUISceneChange.REGISTER2.changeScene((Stage)next.getScene().getWindow());
+		if (!name.isEmpty() && !pass.isEmpty() && !repass.isEmpty()) {
+			if (pass.equals(repass) && pass.length() >= 6) {
+				GameUISceneChange.REGISTER2.changeScene((Stage) next.getScene().getWindow());
+			}
+		} else {
+			if (name.isEmpty() || name.length() < 8) {
+				if(name.length() < 8) {
+					checkusername.setText("username must contain at least 8 characters");
+					System.out.println("1");
+				}
+				checkusername.setText("");
+				System.out.println("username");
+			}
+			if (pass.isEmpty() || pass.length() < 8) {
+				if(pass.length() < 8) {
+					checkpass.setText("password must contain at least 8 characters");
+					System.out.println("2");
+				}
+				checkpass.setText("");
+				System.out.println("password");
+			}
+			if (repass.isEmpty() || repass.length() < 8) {
+				if(!repass.equals(pass)){
+					checkrepass.setText("repassword is not a same as password.");
+					System.out.println("3");
+				}
+				checkrepass.setText("");
+				System.out.println("repass");
+			}
 		}
-		if(!name.equals("")) {
-			username.setStyle("-fx-text-inner-color: red;");
-			username.setText("Insert username");
-		}
-		if(!pass.equals("")) {
-			password.setStyle("-fx-text-inner-color: red;");
-			password.setText("Insert password");
-		}
-		if(!repass.equals("")) {
-			repassword.setStyle("-fx-text-inner-color: red;");
-			repassword.setText("Insert repassword");
-		}
-		username.setText("-fx-text-inner-color: black;");
-		password.setText("-fx-text-inner-color: black;");
-		repassword.setText("-fx-text-inner-color: black;");
 	}
-	
+
 	public void onCancelButton(ActionEvent event) {
-		GameUISceneChange.LOGIN.changeScene((Stage)cancel.getScene().getWindow());
+		GameUISceneChange.LOGIN.changeScene((Stage) cancel.getScene().getWindow());
 	}
 }
